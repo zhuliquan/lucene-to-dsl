e9e910ef07651770733b177d9aa0f2114417f664
zhuliquan <zlq164114@sina.com>
2021-11-22 01:19:34 +0800

    feature: split bool oper to and_symbol and or_symbol and not_symbol

--- internal/query/bool_oper.go
+++ internal/query/bool_oper.go
@@ -1,30 +1,52 @@
 package query
 
 // bool operator ("AND" / "OR" / "NOT") or ("and" / "or" / "not") or ("&&" / "||" / "!")
-type BoolOper struct {
-	AndSymbol string `parser:"  WHITESPACE+ @AND AND WHITESPACE+" json:"and_symbol"`
-	OrSymbol  string `parser:"| WHITESPACE+ @OR OR WHITESPACE+" json:"or_symbol"`
-	NotSymbol string `parser:"| @NOT WHITESPACE+" json:"not_symbol"`
-	AndIdent  string `parser:"| WHITESPACE+ @('AND' | 'and') WHITESPACE+" json:"and_ident"`
-	OrIdent   string `parser:"| WHITESPACE+ @('OR' | 'or') WHITESPACE+" json:"or_ident"`
-	NotIdent  string `parser:"| @('NOT' | 'not') WHITESPACE+" json:"not_ident"`
+type ANDSymbol struct {
+	SAND string `parser:"  WHITESPACE+ @(AND AND) WHITESPACE+" json:"sand"`
+	LAND string `parser:"| WHITESPACE+ @('AND' | 'and') WHITESPACE+" json:"land"`
 }
 
-func (o *BoolOper) String() string {
+func (o *ANDSymbol) String() string {
 	if o == nil {
 		return ""
-	} else if o.AndSymbol != "" {
-		return "AND"
-	} else if o.AndIdent != "" {
-		return "AND"
-	} else if o.OrIdent != "" {
-		return "OR"
-	} else if o.OrSymbol != "" {
-		return "OR"
-	} else if o.NotIdent != "" {
-		return "NOT"
-	} else if o.NotSymbol != "" {
-		return "NOT"
+	} else if o.SAND != "" {
+		return o.SAND
+	} else if o.LAND != "" {
+		return o.LAND
+	} else {
+		return ""
+	}
+}
+
+type ORSymbol struct {
+	SOR string `parser:"  WHITESPACE+ @(OR OR) WHITESPACE+" json:"sor"`
+	LOR string `parser:"| WHITESPACE+ @('OR' | 'or') WHITESPACE+" json:"lor"`
+}
+
+func (o *ORSymbol) String() string {
+	if o == nil {
+		return ""
+	} else if o.SOR != "" {
+		return o.SOR
+	} else if o.LOR != "" {
+		return o.LOR
+	} else {
+		return ""
+	}
+}
+
+type NOTSymbol struct {
+	SNOT string `parser:"  @NOT WHITESPACE+" json:"snot"`
+	LNOT string `parser:"| @('NOT' | 'not') WHITESPACE+" json:"lnot"`
+}
+
+func (o *NOTSymbol) String() string {
+	if o == nil {
+		return ""
+	} else if o.SNOT != "" {
+		return "!"
+	} else if o.LNOT != "" {
+		return o.LNOT
 	} else {
 		return ""
 	}
--- internal/query/bool_oper_test.go
+++ internal/query/bool_oper_test.go
@@ -9,72 +9,99 @@ import (
 
 func TestBoolOperator(t *testing.T) {
 
-	var boolOperParser = participle.MustBuild(
-		&BoolOper{},
+	var andOperParser = participle.MustBuild(
+		&ANDSymbol{},
+		participle.Lexer(Lexer),
+	)
+	var orOperParser = participle.MustBuild(
+		&ORSymbol{},
+		participle.Lexer(Lexer),
+	)
+	var notOperParser = participle.MustBuild(
+		&NOTSymbol{},
 		participle.Lexer(Lexer),
 	)
 
 	type testCase struct {
-		name  string
-		input string
-		want  *BoolOper
+		name    string
+		parser  interface{}
+		boolObj interface{}
+		input   string
+		want    interface{}
 	}
 
 	var testCases = []testCase{
 		{
-			name:  "TestBoolOper01",
-			input: ` AND   `,
-			want:  &BoolOper{AndIdent: "AND"},
+			name:    "TestBoolOper01",
+			parser:  andOperParser,
+			boolObj: &ANDSymbol{},
+			input:   ` AND   `,
+			want:    &ANDSymbol{LAND: "AND"},
 		},
 		{
-			name:  "TestBoolOper02",
-			input: ` and `,
-			want:  &BoolOper{AndIdent: "and"},
+			name:    "TestBoolOper02",
+			parser:  andOperParser,
+			boolObj: &ANDSymbol{},
+			input:   ` and `,
+			want:    &ANDSymbol{LAND: "and"},
 		},
 		{
-			name:  "TestBoolOper03",
-			input: ` && `,
-			want:  &BoolOper{AndSymbol: "&"},
+			name:    "TestBoolOper03",
+			parser:  andOperParser,
+			boolObj: &ANDSymbol{},
+			input:   ` && `,
+			want:    &ANDSymbol{SAND: "&&"},
 		},
 		{
-			name:  "TestBoolOper04",
-			input: ` OR  `,
-			want:  &BoolOper{OrIdent: "OR"},
+			name:    "TestBoolOper04",
+			parser:  orOperParser,
+			boolObj: &ORSymbol{},
+			input:   ` OR  `,
+			want:    &ORSymbol{LOR: "OR"},
 		},
 		{
-			name:  "TestBoolOper05",
-			input: ` or  `,
-			want:  &BoolOper{OrIdent: "or"},
+			name:    "TestBoolOper05",
+			parser:  orOperParser,
+			boolObj: &ORSymbol{},
+			input:   ` or  `,
+			want:    &ORSymbol{LOR: "or"},
 		},
 		{
-			name:  "TestBoolOper06",
-			input: ` ||  `,
-			want:  &BoolOper{OrSymbol: "|"},
+			name:    "TestBoolOper06",
+			parser:  orOperParser,
+			boolObj: &ORSymbol{},
+			input:   ` ||  `,
+			want:    &ORSymbol{SOR: "||"},
 		},
 		{
-			name:  "TestBoolOper07",
-			input: `NOT `,
-			want:  &BoolOper{NotIdent: "NOT"},
+			name:    "TestBoolOper07",
+			parser:  notOperParser,
+			boolObj: &NOTSymbol{},
+			input:   `NOT `,
+			want:    &NOTSymbol{LNOT: "NOT"},
 		},
 		{
-			name:  "TestBoolOper08",
-			input: `not `,
-			want:  &BoolOper{NotIdent: "not"},
+			name:    "TestBoolOper08",
+			parser:  notOperParser,
+			boolObj: &NOTSymbol{},
+			input:   `not `,
+			want:    &NOTSymbol{LNOT: "not"},
 		},
 		{
-			name:  "TestBoolOper09",
-			input: `! `,
-			want:  &BoolOper{NotSymbol: "!"},
+			name:    "TestBoolOper09",
+			parser:  notOperParser,
+			boolObj: &NOTSymbol{},
+			input:   `! `,
+			want:    &NOTSymbol{SNOT: "!"},
 		},
 	}
 
 	for _, tt := range testCases {
 		t.Run(tt.name, func(t *testing.T) {
-			var out = &BoolOper{}
-			if err := boolOperParser.ParseString(tt.input, out); err != nil {
+			if err := tt.parser.(*participle.Parser).ParseString(tt.input, tt.boolObj); err != nil {
 				t.Errorf("failed to parse input: %s, err: %+v", tt.input, err)
-			} else if !reflect.DeepEqual(out, tt.want) {
-				t.Errorf("ParseString( %s ) = %+v, want: %+v", tt.input, out, tt.want)
+			} else if !reflect.DeepEqual(tt.boolObj, tt.want) {
+				t.Errorf("ParseString( %s ) = %+v, want: %+v", tt.input, tt.boolObj, tt.want)
 			}
 		})
 	}
