e8c3e5720a6b136c7ccc3916d0c12de6afd39732
zhuliquan <zlq164114@sina.com>
2021-11-22 01:20:18 +0800

    feature: add lucene query difination

--- internal/query/lucene.go
+++ internal/query/lucene.go
@@ -1,5 +1,40 @@
 package query
 
 type Lucene struct {
-	
+	OrQuery *OrQuery  `parser:"@@" json:"or_query"`
+	OrTerms []*OrTerm `parser:"@@*" json:"or_terms"`
+}
+
+type OrTerm struct {
+	ORSymbol  *ORSymbol  `parser:"@@" json:"or_symbol"`
+	NOTSymbol *NOTSymbol `parser:"@@?" json:"not_symbol"`
+	OrQuery   *OrQuery   `parser:"@@" json:"or_query"`
+}
+
+type OrQuery struct {
+	AndQuery *AndQuery  `parser:"@@" json:"and_query"`
+	AndTerms []*AndTerm `parser:"@@*" json:"and_terms" `
+}
+
+type AndTerm struct {
+	ANDSymbol *ANDSymbol `parser:"@@" json:"and_symbol"`
+	NOTSymbol *NOTSymbol `parser:"@@?" json:"not_symbol"`
+	AndQuery  *AndQuery  `parser:"@@" json:"and_query"`
+}
+
+type AndQuery struct {
+	NotQuery   *NotQuery   `parser:"  @@" json:"not_query"`
+	ParenQuery *ParenQuery `parser:"| @@" json:"paren_query"`
+	FieldQuery *FieldTerm  `parser:"| @@" json:"field_query"`
+}
+
+type NotQuery struct {
+	NOTSymbol *NOTSymbol `parser:"@@" json:"not_symbol"`
+	SubQuery  *Lucene    `parser:"@@" json:"sub_query"`
+}
+
+type ParenQuery struct {
+	LParen   string  `parser:"@LPAREN" json:"lparen"`
+	SubQuery *Lucene `parser:"@@" json:"sub_query"`
+	RParen   string  `parser:"@RPAREN" json:"rparen"`
 }
--- internal/query/lucene_test.go
+++ internal/query/lucene_test.go
@@ -1,1 +1,47 @@
 package query
+
+import (
+	"testing"
+
+	"github.com/alecthomas/participle"
+)
+
+func TestLucene(t *testing.T) {
+	var luceneParser = participle.MustBuild(
+		&Lucene{},
+		participle.Lexer(Lexer),
+	)
+
+	type testCase struct {
+		name    string
+		input   string
+		wantErr bool
+	}
+
+	var testCases = []testCase{
+		{
+			name:    "TestLucene01",
+			input:   `x:1 AND NOT x:2`,
+			wantErr: false,
+		},
+		{
+			name:    "TestLucene02",
+			input:   `NOT (x:1 AND y:2) OR z:9`,
+			wantErr: false,
+		},
+		{
+			name:    "TestLucne03",
+			input:   `(x:1 AND NOT y:2) AND (NOT x:8 AND k:90)`,
+			wantErr: false,
+		},
+	}
+
+	for _, tt := range testCases {
+		t.Run(tt.name, func(t *testing.T) {
+			var lucene = &Lucene{}
+			if err := luceneParser.ParseString(tt.input, lucene); (err != nil) != tt.wantErr {
+				t.Errorf("parser lucene, err: %+v", err)
+			}
+		})
+	}
+}
