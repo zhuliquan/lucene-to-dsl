a3a78f23a1bddc01bd06174c8c3683031adc71db
zhuliquan <zlq164114@sina.com>
2021-11-25 01:31:29 +0800

    feature: add sinlge term file include simple_term phrase_term regexp_term double_side_range_term single_side_range_term

---
+++ internal/term/single_term.go
@@ -0,0 +1,309 @@
+package term
+
+import (
+	"fmt"
+	"strings"
+)
+
+// simple term: is a single term without escape char and whitespace
+type SimpleTerm struct {
+	Value []string `parser:"@(IDENT|WILDCARD)+" json:"value"`
+}
+
+func (t *SimpleTerm) ValueS() string {
+	if t == nil {
+		return ""
+	} else {
+		return strings.Join(t.Value, "")
+	}
+}
+
+func (t *SimpleTerm) String() string {
+	if t == nil {
+		return ""
+	} else {
+		return strings.Join(t.Value, "")
+	}
+}
+
+func (t *SimpleTerm) haveWildcard() bool {
+	if t == nil {
+		return false
+	}
+	for i := 0; i < len(t.Value); i++ {
+		if t.Value[i] == "?" || t.Value[i] == "*" {
+			return true
+		}
+	}
+	return false
+}
+
+// func (t *SimpleTerm) fuzziness() int {
+// 	if t == nil || len(t.Fuzzy) == 0 {
+// 		return 0
+// 	} else if t.Fuzzy == "~" {
+// 		return 1
+// 	} else {
+// 		var v, _ = strconv.Atoi(t.Fuzzy[1:])
+// 		return v
+// 	}
+// }
+
+// func (t *SimpleTerm) boost() float64 {
+// 	if t == nil {
+// 		return 0.0
+// 	} else if len(t.Boost) != 0 {
+// 		var v, _ = strconv.ParseFloat(t.Boost[1:], 64)
+// 		return v
+// 	} else {
+// 		return 1.0
+// 	}
+// }
+
+// phrase term: a series of terms be surrounded with quotation, for instance "foo bar".
+type PhraseTerm struct {
+	Value string `parser:"@STRING" json:"value"`
+}
+
+func (t *PhraseTerm) Values() string {
+	if t == nil || len(t.Value) == 0 {
+		return ""
+	} else {
+		return t.Value[1 : len(t.Value)-1]
+	}
+}
+
+func (t *PhraseTerm) String() string {
+	if t == nil {
+		return ""
+	} else {
+		return t.Value
+	}
+}
+
+func (t *PhraseTerm) haveWildcard() bool {
+	if t == nil {
+		return false
+	}
+	for i := 1; i < len(t.Value)-1; i++ {
+		if i > 1 && (t.Value[i] == '?' || t.Value[i] == '*' && t.Value[i-1] != '\\') {
+			return true
+		}
+		if i == 1 && (t.Value[i] == '?' || t.Value[i] == '*') {
+			return true
+		}
+	}
+	return false
+}
+
+// func (t *PhraseTerm) fuzziness() int {
+// 	if t == nil || len(t.Fuzzy) == 0 {
+// 		return 0
+// 	} else if t.Fuzzy == "~" {
+// 		return 1
+// 	} else {
+// 		var v, _ = strconv.Atoi(t.Fuzzy[1:])
+// 		return v
+// 	}
+// }
+
+// func (t *PhraseTerm) boost() float64 {
+// 	if t == nil {
+// 		return 0.0
+// 	} else if len(t.Boost) != 0 {
+// 		var v, _ = strconv.ParseFloat(t.Boost[1:], 64)
+// 		return v
+// 	} else {
+// 		return 1.0
+// 	}
+// }
+
+// a regexp term is surrounded be slash, for instance /\d+\.?\d+/ in here if you want present '/' you should type '\/'
+type RegexpTerm struct {
+	Value string `parser:"@REGEXP" json:"value"`
+}
+
+func (t *RegexpTerm) ValuesS() string {
+	if t == nil || len(t.Value) == 0 {
+		return ""
+	} else {
+		return t.Value[1 : len(t.Value)-1]
+	}
+}
+
+func (t *RegexpTerm) String() string {
+	if t == nil {
+		return ""
+	} else {
+		return t.Value
+	}
+}
+
+// range bound like this [1, 2] [1, 2) (1, 2] (1, 2)
+type Bound struct {
+	LeftInclude  *RangeValue `json:"left_include"`
+	LeftExclude  *RangeValue `json:"left_exclude"`
+	RightInclude *RangeValue `json:"right_include"`
+	RightExclude *RangeValue `json:"right_exclude"`
+}
+
+type RangeValue struct {
+	PhraseValue string   `parser:"  @STRING" json:"phrase_value"`
+	InfinityVal string   `parser:"| @('*')" json:"infinity_val"`
+	SimpleValue []string `parser:"| @(IDENT|PLUS|MINUS)+" json:"simple_value"`
+}
+
+func (v *RangeValue) String() string {
+	if v == nil {
+		return ""
+	} else if len(v.PhraseValue) != 0 {
+		return v.PhraseValue
+	} else if len(v.InfinityVal) != 0 {
+		return v.InfinityVal
+	} else if len(v.SimpleValue) != 0 {
+		return strings.Join(v.SimpleValue, "")
+	} else {
+		return ""
+	}
+}
+
+//double side of range term: a term is surrounded by brace / bracket, for instance [1 TO 2] / [1 TO 2} / {1 TO 2] / {1 TO 2}
+type DRangeTerm struct {
+	LBRACKET string      `parser:"@(LBRACE|LBRACK) WHITESPACE*" json:"left_bracket"`
+	LValue   *RangeValue `parser:"@@" json:"left_value"`
+	TO       string      `parser:"WHITESPACE+ @('TO') WHITESPACE+"`
+	RValue   *RangeValue `parser:"@@" json:"right_value"`
+	RBRACKET string      `parser:"WHITESPACE* @(RBRACK|RBRACE)" json:"right_bracket"`
+}
+
+func (t *DRangeTerm) ToBound() *Bound {
+	if t == nil {
+		return nil
+	} else if t.LBRACKET == "[" || t.RBRACKET == "]" {
+		return &Bound{LeftInclude: t.LValue, RightInclude: t.RValue}
+	} else if t.LBRACKET == "[" || t.RBRACKET == "}" {
+		return &Bound{LeftInclude: t.LValue, RightExclude: t.RValue}
+	} else if t.LBRACKET == "{" || t.RBRACKET == "]" {
+		return &Bound{LeftExclude: t.LValue, RightInclude: t.RValue}
+	} else if t.LBRACKET == "{" || t.RBRACKET == "}" {
+		return &Bound{LeftExclude: t.LValue, RightExclude: t.RValue}
+	} else {
+		return nil
+	}
+}
+
+func (t *DRangeTerm) String() string {
+	if t == nil {
+		return ""
+	} else {
+		return fmt.Sprintf("%s %s TO %s %s", t.LBRACKET, t.LValue.String(), t.RValue.String(), t.RBRACKET)
+	}
+}
+
+// single side of range term: a term is behind of symbol ('>' / '<' / '>=' / '<=')
+type SRangeTerm struct {
+	Symbol     string      `parser:"@COMPARE" json:"symbol"`
+	SimpleTerm *SimpleTerm `parser:"( @@ " json:"simple_term"`
+	PhraseTerm *PhraseTerm `parser:"| @@)" json:"phrase_term"`
+}
+
+func (t *SRangeTerm) toDRangeTerm() *DRangeTerm {
+	if t == nil {
+		return nil
+	} else if t.Symbol == ">" && t.SimpleTerm != nil {
+		return &DRangeTerm{LBRACKET: "{", LValue: &RangeValue{SimpleValue: t.SimpleTerm.Value}, TO: "TO", RValue: &RangeValue{InfinityVal: "*"}, RBRACKET: "}"}
+	} else if t.Symbol == ">" && t.PhraseTerm != nil {
+		return &DRangeTerm{LBRACKET: "{", LValue: &RangeValue{PhraseValue: t.PhraseTerm.Value}, TO: "TO", RValue: &RangeValue{InfinityVal: "*"}, RBRACKET: "}"}
+	} else if t.Symbol == ">=" && t.SimpleTerm != nil {
+		return &DRangeTerm{LBRACKET: "[", LValue: &RangeValue{SimpleValue: t.SimpleTerm.Value}, TO: "TO", RValue: &RangeValue{InfinityVal: "*"}, RBRACKET: "}"}
+	} else if t.Symbol == ">=" && t.PhraseTerm != nil {
+		return &DRangeTerm{LBRACKET: "[", LValue: &RangeValue{PhraseValue: t.PhraseTerm.Value}, TO: "TO", RValue: &RangeValue{InfinityVal: "*"}, RBRACKET: "}"}
+	} else if t.Symbol == "<" && t.SimpleTerm != nil {
+		return &DRangeTerm{LBRACKET: "{", LValue: &RangeValue{InfinityVal: "*"}, TO: "TO", RValue: &RangeValue{SimpleValue: t.SimpleTerm.Value}, RBRACKET: "}"}
+	} else if t.Symbol == "<" && t.PhraseTerm != nil {
+		return &DRangeTerm{LBRACKET: "{", LValue: &RangeValue{InfinityVal: "*"}, TO: "TO", RValue: &RangeValue{PhraseValue: t.PhraseTerm.Value}, RBRACKET: "}"}
+	} else if t.Symbol == "<=" && t.SimpleTerm != nil {
+		return &DRangeTerm{LBRACKET: "{", LValue: &RangeValue{InfinityVal: "*"}, TO: "TO", RValue: &RangeValue{SimpleValue: t.SimpleTerm.Value}, RBRACKET: "]"}
+	} else if t.Symbol == "<=" && t.PhraseTerm != nil {
+		return &DRangeTerm{LBRACKET: "{", LValue: &RangeValue{InfinityVal: "*"}, TO: "TO", RValue: &RangeValue{PhraseValue: t.PhraseTerm.Value}, RBRACKET: "]"}
+	} else {
+		return nil
+	}
+}
+
+func (t *SRangeTerm) ToBound() *Bound {
+	return t.toDRangeTerm().ToBound()
+}
+
+func (t *SRangeTerm) String() string {
+	return t.toDRangeTerm().String()
+}
+
+type RangeTerm struct {
+	SRangeTerm *SRangeTerm `parser:"  @@" json:"s_range_term"`
+	DRangeTerm *DRangeTerm `parser:"| @@" json:"d_range_term"`
+}
+
+func (t *RangeTerm) String() string {
+	if t == nil {
+		return ""
+	} else if t.SRangeTerm != nil {
+		return t.SRangeTerm.String()
+	} else if t.DRangeTerm != nil {
+		return t.DRangeTerm.String()
+	} else {
+		return ""
+	}
+}
+
+func (t *RangeTerm) ToBound() *Bound {
+	if t == nil {
+		return nil
+	} else if t.SRangeTerm != nil {
+		return t.SRangeTerm.ToBound()
+	} else if t.DRangeTerm != nil {
+		return t.DRangeTerm.ToBound()
+	} else {
+		return nil
+	}
+}
+
+// func (t *SRangeTerm) isRange() bool {
+// 	return t != nil && len(t.Symbol) != 0
+// }
+
+// func (t *SRangeTerm) haveWildcard() bool {
+// 	if t == nil {
+// 		return false
+// 	} else if t.PhraseTerm != nil {
+// 		return t.PhraseTerm.haveWildcard()
+// 	} else if t.SimpleTerm != nil {
+// 		return t.SimpleTerm.haveWildcard()
+// 	} else {
+// 		return false
+// 	}
+// }
+
+// func (t *SRangeTerm) fuzziness() int {
+// 	if t == nil {
+// 		return 0
+// 	} else if t.PhraseTerm != nil {
+// 		return t.PhraseTerm.fuzziness()
+// 	} else if t.SimpleTerm != nil {
+// 		return t.SimpleTerm.fuzziness()
+// 	} else {
+// 		return 0
+// 	}
+// }
+
+// func (t *SRangeTerm) boost() float64 {
+// 	if t == nil {
+// 		return 0.0
+// 	} else if t.PhraseTerm != nil {
+// 		return t.PhraseTerm.boost()
+// 	} else if t.SimpleTerm != nil {
+// 		return t.SimpleTerm.boost()
+// 	} else {
+// 		return 1.0
+// 	}
+// }
---
+++ internal/term/single_term_test.go
@@ -0,0 +1,358 @@
+package term
+
+import (
+	"reflect"
+	"testing"
+
+	"github.com/alecthomas/participle"
+	"github.com/zhuliquan/lucene-to-dsl/internal/token"
+)
+
+func TestSimpleTerm(t *testing.T) {
+	var simpleTermParser = participle.MustBuild(
+		&SimpleTerm{},
+		participle.Lexer(token.Lexer),
+	)
+
+	type testCase struct {
+		name  string
+		input string
+		want  *SimpleTerm
+	}
+	var testCases = []testCase{
+		{
+			name:  "TestSimpleTerm01",
+			input: `\/dsada\/\ dasda80980?*`,
+			want:  &SimpleTerm{Value: []string{`\/dsada\/\ dasda80980`, `?`, `*`}},
+		},
+	}
+
+	for _, tt := range testCases {
+		t.Run(tt.name, func(t *testing.T) {
+			var out = &SimpleTerm{}
+			if err := simpleTermParser.ParseString(tt.input, out); err != nil {
+				t.Errorf("failed to parse input: %s, err: %+v", tt.input, err)
+			} else if !reflect.DeepEqual(tt.want, out) {
+				t.Errorf("simpleTermParser.ParseString( %s ) = %+v, want: %+v", tt.input, out, tt.want)
+			}
+		})
+	}
+}
+
+func TestPhraseTerm(t *testing.T) {
+	var phraseTermParser = participle.MustBuild(
+		&PhraseTerm{},
+		participle.Lexer(token.Lexer),
+	)
+
+	type testCase struct {
+		name  string
+		input string
+		want  *PhraseTerm
+	}
+	var testCases = []testCase{
+		{
+			name:  "PhraseTerm01",
+			input: `"dsada 78"`,
+			want:  &PhraseTerm{Value: `"dsada 78"`},
+		},
+	}
+
+	for _, tt := range testCases {
+		t.Run(tt.name, func(t *testing.T) {
+			var out = &PhraseTerm{}
+			if err := phraseTermParser.ParseString(tt.input, out); err != nil {
+				t.Errorf("failed to parse input: %s, err: %+v", tt.input, err)
+			} else if !reflect.DeepEqual(tt.want, out) {
+				t.Errorf("phraseTermParser.ParseString( %s ) = %+v, want: %+v", tt.input, out, tt.want)
+			}
+		})
+	}
+}
+
+func TestRegexpTerm(t *testing.T) {
+	var regexpTermParser = participle.MustBuild(
+		&RegexpTerm{},
+		participle.Lexer(token.Lexer),
+	)
+
+	type testCase struct {
+		name  string
+		input string
+		want  *RegexpTerm
+	}
+	var testCases = []testCase{
+		{
+			name:  "RegexpTerm01",
+			input: `/dsada 78/`,
+			want:  &RegexpTerm{Value: `/dsada 78/`},
+		},
+		{
+			name:  "RegexpTerm02",
+			input: `/\d+\/\d+\.\d+.+/`,
+			want:  &RegexpTerm{Value: `/\d+\/\d+\.\d+.+/`},
+		},
+	}
+
+	for _, tt := range testCases {
+		t.Run(tt.name, func(t *testing.T) {
+			var out = &RegexpTerm{}
+			if err := regexpTermParser.ParseString(tt.input, out); err != nil {
+				t.Errorf("failed to parse input: %s, err: %+v", tt.input, err)
+			} else if !reflect.DeepEqual(tt.want, out) {
+				t.Errorf("regexpTermParser.ParseString( %s ) = %+v, want: %+v", tt.input, out, tt.want)
+			}
+		})
+	}
+}
+
+func TestDRangeTerm(t *testing.T) {
+	var rangeTermParser = participle.MustBuild(
+		&DRangeTerm{},
+		participle.Lexer(token.Lexer),
+	)
+
+	type testCase struct {
+		name  string
+		input string
+		want  *DRangeTerm
+	}
+	var testCases = []testCase{
+		{
+			name:  "DRangeTerm01",
+			input: `[1 TO 2]`,
+			want: &DRangeTerm{
+				LBRACKET: "[",
+				LValue:   &RangeValue{SimpleValue: []string{"1"}},
+				TO:       "TO",
+				RValue:   &RangeValue{SimpleValue: []string{"2"}},
+				RBRACKET: "]"},
+		},
+		{
+			name:  "DRangeTerm02",
+			input: `[1 TO 2 }`,
+			want: &DRangeTerm{
+				LBRACKET: "[",
+				LValue:   &RangeValue{SimpleValue: []string{"1"}},
+				TO:       "TO",
+				RValue:   &RangeValue{SimpleValue: []string{"2"}},
+				RBRACKET: "}",
+			},
+		},
+		{
+			name:  `DRangeTerm03`,
+			input: `{ 1 TO 2}`,
+			want: &DRangeTerm{
+				LBRACKET: "{",
+				LValue:   &RangeValue{SimpleValue: []string{"1"}},
+				TO:       "TO",
+				RValue:   &RangeValue{SimpleValue: []string{"2"}},
+				RBRACKET: "}",
+			},
+		},
+		{
+			name:  `DRangeTerm04`,
+			input: `{ 1 TO 2]`,
+			want: &DRangeTerm{
+				LBRACKET: "{",
+				LValue:   &RangeValue{SimpleValue: []string{"1"}},
+				TO:       "TO",
+				RValue:   &RangeValue{SimpleValue: []string{"2"}},
+				RBRACKET: "]",
+			},
+		},
+		{
+			name:  `DRangeTerm05`,
+			input: `[10 TO *]`,
+			want: &DRangeTerm{
+				LBRACKET: "[",
+				LValue:   &RangeValue{SimpleValue: []string{"10"}},
+				TO:       "TO",
+				RValue:   &RangeValue{InfinityVal: "*"},
+				RBRACKET: "]",
+			},
+		},
+		{
+			name:  `DRangeTerm06`,
+			input: `{* TO 2012-01-01}`,
+			want: &DRangeTerm{
+				LBRACKET: "{",
+				LValue:   &RangeValue{InfinityVal: "*"},
+				TO:       "TO",
+				RValue:   &RangeValue{SimpleValue: []string{"2012", "-", "01", "-", "01"}},
+				RBRACKET: "}",
+			},
+		},
+		{
+			name:  `DRangeTerm07`,
+			input: `{* TO "2012-01-01 09:08:16"}`,
+			want: &DRangeTerm{
+				LBRACKET: "{",
+				LValue:   &RangeValue{InfinityVal: "*"},
+				TO:       "TO",
+				RValue:   &RangeValue{PhraseValue: "\"2012-01-01 09:08:16\""},
+				RBRACKET: "}",
+			},
+		},
+	}
+
+	for _, tt := range testCases {
+		t.Run(tt.name, func(t *testing.T) {
+			var out = &DRangeTerm{}
+			if err := rangeTermParser.ParseString(tt.input, out); err != nil {
+				t.Errorf("failed to parse input: %s, err: %+v", tt.input, err)
+			} else if !reflect.DeepEqual(tt.want, out) {
+				t.Errorf("rangeTermParser.ParseString( %s ) = %+v, want: %+v", tt.input, out, tt.want)
+			}
+		})
+	}
+}
+
+func TestSRangeTerm(t *testing.T) {
+	var rangesTermParser = participle.MustBuild(
+		&SRangeTerm{},
+		participle.Lexer(token.Lexer),
+	)
+
+	type testCase struct {
+		name  string
+		input string
+		want  *SRangeTerm
+	}
+	var testCases = []testCase{
+		{
+			name:  "SRangeTerm01",
+			input: `<="dsada 78"`,
+			want:  &SRangeTerm{Symbol: "<=", PhraseTerm: &PhraseTerm{Value: `"dsada 78"`}},
+		},
+		{
+			name:  "SRangeTerm05",
+			input: `<=dsada\ 78`,
+			want:  &SRangeTerm{Symbol: "<=", SimpleTerm: &SimpleTerm{Value: []string{`dsada\ 78`}}},
+		},
+	}
+
+	for _, tt := range testCases {
+		t.Run(tt.name, func(t *testing.T) {
+			var out = &SRangeTerm{}
+			if err := rangesTermParser.ParseString(tt.input, out); err != nil {
+				t.Errorf("failed to parse input: %s, err: %+v", tt.input, err)
+			} else if !reflect.DeepEqual(tt.want, out) {
+				t.Errorf("rangesTermParser.ParseString( %s ) = %+v, want: %+v", tt.input, out, tt.want)
+			}
+		})
+	}
+}
+
+func TestRangeTerm(t *testing.T) {
+	var rangesTermParser = participle.MustBuild(
+		&RangeTerm{},
+		participle.Lexer(token.Lexer),
+	)
+
+	type testCase struct {
+		name  string
+		input string
+		want  *RangeTerm
+	}
+	var testCases = []testCase{
+		{
+			name:  "RangeTerm01",
+			input: `<="dsada 78"`,
+			want:  &RangeTerm{SRangeTerm: &SRangeTerm{Symbol: "<=", PhraseTerm: &PhraseTerm{Value: `"dsada 78"`}}},
+		},
+		{
+			name:  "RangeTerm02",
+			input: `<=dsada\ 78`,
+			want:  &RangeTerm{SRangeTerm: &SRangeTerm{Symbol: "<=", SimpleTerm: &SimpleTerm{Value: []string{`dsada\ 78`}}}},
+		},
+		{
+			name:  "RangeTerm03",
+			input: `[1 TO 2]`,
+			want: &RangeTerm{DRangeTerm: &DRangeTerm{
+				LBRACKET: "[",
+				LValue:   &RangeValue{SimpleValue: []string{"1"}},
+				TO:       "TO",
+				RValue:   &RangeValue{SimpleValue: []string{"2"}},
+				RBRACKET: "]",
+			}},
+		},
+		{
+			name:  "RangeTerm04",
+			input: `[1 TO 2 }`,
+			want: &RangeTerm{DRangeTerm: &DRangeTerm{
+				LBRACKET: "[",
+				LValue:   &RangeValue{SimpleValue: []string{"1"}},
+				TO:       "TO",
+				RValue:   &RangeValue{SimpleValue: []string{"2"}},
+				RBRACKET: "}",
+			}},
+		},
+		{
+			name:  `RangeTerm05`,
+			input: `{ 1 TO 2}`,
+			want: &RangeTerm{DRangeTerm: &DRangeTerm{
+				LBRACKET: "{",
+				LValue:   &RangeValue{SimpleValue: []string{"1"}},
+				TO:       "TO",
+				RValue:   &RangeValue{SimpleValue: []string{"2"}},
+				RBRACKET: "}",
+			}},
+		},
+		{
+			name:  `RangeTerm06`,
+			input: `{ 1 TO 2]`,
+			want: &RangeTerm{DRangeTerm: &DRangeTerm{
+				LBRACKET: "{",
+				LValue:   &RangeValue{SimpleValue: []string{"1"}},
+				TO:       "TO",
+				RValue:   &RangeValue{SimpleValue: []string{"2"}},
+				RBRACKET: "]",
+			}},
+		},
+		{
+			name:  `RangeTerm07`,
+			input: `[10 TO *]`,
+			want: &RangeTerm{DRangeTerm: &DRangeTerm{
+				LBRACKET: "[",
+				LValue:   &RangeValue{SimpleValue: []string{"10"}},
+				TO:       "TO",
+				RValue:   &RangeValue{InfinityVal: "*"},
+				RBRACKET: "]",
+			}},
+		},
+		{
+			name:  `RangeTerm08`,
+			input: `{* TO 2012-01-01}`,
+			want: &RangeTerm{DRangeTerm: &DRangeTerm{
+				LBRACKET: "{",
+				LValue:   &RangeValue{InfinityVal: "*"},
+				TO:       "TO",
+				RValue:   &RangeValue{SimpleValue: []string{"2012", "-", "01", "-", "01"}},
+				RBRACKET: "}",
+			}},
+		},
+		{
+			name:  `RangeTerm09`,
+			input: `{* TO "2012-01-01 09:08:16"}`,
+			want: &RangeTerm{DRangeTerm: &DRangeTerm{
+				LBRACKET: "{",
+				LValue:   &RangeValue{InfinityVal: "*"},
+				TO:       "TO",
+				RValue:   &RangeValue{PhraseValue: "\"2012-01-01 09:08:16\""},
+				RBRACKET: "}",
+			}},
+		},
+	}
+
+	for _, tt := range testCases {
+		t.Run(tt.name, func(t *testing.T) {
+			var out = &RangeTerm{}
+			if err := rangesTermParser.ParseString(tt.input, out); err != nil {
+				t.Errorf("failed to parse input: %s, err: %+v", tt.input, err)
+			} else if !reflect.DeepEqual(tt.want, out) {
+				t.Errorf("rangesTermParser.ParseString( %s ) = %+v, want: %+v", tt.input, out, tt.want)
+			}
+		})
+	}
+}
