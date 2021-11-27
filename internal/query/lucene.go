7a9985da865e78345dbb1d31edaba809db379fff
zhuliquan <zlq164114@sina.com>
2021-11-27 11:14:49 +0800

    fix: fix operator package refer

--- internal/query/lucene.go
+++ internal/query/lucene.go
@@ -1,14 +1,19 @@
 package query
 
+import (
+	op "github.com/zhuliquan/lucene-to-dsl/internal/operator"
+	"github.com/zhuliquan/lucene-to-dsl/internal/term"
+)
+
 type Lucene struct {
 	OrQuery *OrQuery  `parser:"@@" json:"or_query"`
 	OrTerms []*OrTerm `parser:"@@*" json:"or_terms"`
 }
 
 type OrTerm struct {
-	ORSymbol  *ORSymbol  `parser:"@@" json:"or_symbol"`
-	NOTSymbol *NOTSymbol `parser:"@@?" json:"not_symbol"`
-	OrQuery   *OrQuery   `parser:"@@" json:"or_query"`
+	ORSymbol  *op.ORSymbol  `parser:"@@" json:"or_symbol"`
+	NOTSymbol *op.NOTSymbol `parser:"@@?" json:"not_symbol"`
+	OrQuery   *OrQuery      `parser:"@@" json:"or_query"`
 }
 
 type OrQuery struct {
@@ -17,24 +22,39 @@ type OrQuery struct {
 }
 
 type AndTerm struct {
-	ANDSymbol *ANDSymbol `parser:"@@" json:"and_symbol"`
-	NOTSymbol *NOTSymbol `parser:"@@?" json:"not_symbol"`
-	AndQuery  *AndQuery  `parser:"@@" json:"and_query"`
+	ANDSymbol *op.ANDSymbol `parser:"@@" json:"and_symbol"`
+	NOTSymbol *op.NOTSymbol `parser:"@@?" json:"not_symbol"`
+	AndQuery  *AndQuery     `parser:"@@" json:"and_query"`
 }
 
 type AndQuery struct {
 	NotQuery   *NotQuery   `parser:"  @@" json:"not_query"`
 	ParenQuery *ParenQuery `parser:"| @@" json:"paren_query"`
-	FieldQuery *FieldTerm  `parser:"| @@" json:"field_query"`
+	FieldQuery *FieldQuery `parser:"| @@" json:"field_query"`
 }
 
 type NotQuery struct {
-	NOTSymbol *NOTSymbol `parser:"@@" json:"not_symbol"`
-	SubQuery  *Lucene    `parser:"@@" json:"sub_query"`
+	NOTSymbol *op.NOTSymbol `parser:"@@" json:"not_symbol"`
+	SubQuery  *Lucene       `parser:"@@" json:"sub_query"`
 }
 
 type ParenQuery struct {
 	LParen   string  `parser:"@LPAREN" json:"lparen"`
 	SubQuery *Lucene `parser:"@@" json:"sub_query"`
 	RParen   string  `parser:"@RPAREN" json:"rparen"`
+}
+
+type FieldQuery struct {
+	Field *term.Field `parser:"@@ COLON" json:"field"`
+	Term  *term.Term  `parser:"@@" json:"term"`
+}
+
+func (f *FieldQuery) String() string {
+	if f == nil {
+		return ""
+	} else if f.Field == nil || f.Term == nil {
+		return ""
+	} else {
+		return f.Field.String() + " : " + f.Term.String()
+	}
 }
