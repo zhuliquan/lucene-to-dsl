5966d5aa8c7a842d7e00784ed05c82d4485433dc
zhuliquan <zlq164114@sina.com>
2021-12-01 01:21:42 +0800

    feature: add check func for value check

---
+++ lucene/internal/check/check.go
@@ -0,0 +1,26 @@
+package check
+
+func CheckDateValue(dateValue string) error {
+	return nil
+}
+
+func CheckIntegerValue(intValue string) error {
+	return nil
+}
+
+func CheckIpValue(ipValue string) error {
+
+	return nil
+}
+
+func CheckFloatValue(ipValue string) error {
+	return nil
+}
+
+func CheckKeyword(keyword string) error {
+	return nil
+}
+
+func CheckGeoValue(geoValue string) error {
+	return nil
+}
