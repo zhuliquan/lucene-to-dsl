357aa59ad95af8bbb76413c706227de673391fec
zhuliquan <zlq164114@sina.com>
2021-12-11 01:52:18 +0800

    feature: add bound type

--- internal/bound/const.go
+++ internal/bound/const.go
@@ -3,3 +3,13 @@ package bound
 var (
 	Inf = &RangeValue{InfinityVal: "*"}
 )
+
+type BoundType uint16
+
+const (
+	UNKNOWN_BOUND_TYPE         BoundType = iota
+	LEFT_EXCLUDE_RIGHT_INCLUDE BoundType = iota
+	LEFT_EXCLUDE_RIGHT_EXCLUDE
+	LEFT_INCLUDE_RIGHT_INCLUDE
+	LEFT_INCLUDE_RIGHT_EXCLUDE
+)
