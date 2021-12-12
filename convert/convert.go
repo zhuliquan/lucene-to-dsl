725e1de93dc49a6b18044798b970d5a0a0c49755
zhuliquan <zlq164114@sina.com>
2021-12-12 19:32:28 +0800

    feature: add simple convert func (for op node convert)

--- convert/convert.go
+++ convert/convert.go
@@ -3,54 +3,104 @@ package convert
 import (
 	"github.com/zhuliquan/lucene-to-dsl/dsl"
 	"github.com/zhuliquan/lucene-to-dsl/lucene"
+	"github.com/zhuliquan/lucene-to-dsl/mapping"
 )
 
-type Convert func(lucene.Query) (dsl.DSLNode, error)
+var fm *mapping.Mapping
 
-func LuceneToDSL(q *lucene.Lucene) (dsl.DSLNode, error) {
+func InitConvert(m *mapping.Mapping) {
+	fm = m
+}
+
+func LuceneToDSLNode(q *lucene.Lucene) (dsl.DSLNode, error) {
+	return luceneToDSLNode(q)
+}
+
+func luceneToDSLNode(q *lucene.Lucene) (dsl.DSLNode, error) {
 	if q == nil {
 		return nil, EMPTY_AND_QUERY_ERR
 	}
 
-	return nil, nil
+	if node, err := orQueryToDSLNode(q.OrQuery); err != nil {
+		return nil, err
+	} else {
+		var nodes = []dsl.DSLNode{node}
+		for _, query := range q.OSQuery {
+			if node, err := osQueryToDSLNode(query); err != nil {
+				return nil, err
+			} else {
+				nodes = append(nodes, node)
+			}
+		}
+		return &dsl.OrDSLNode{Nodes: nodes}, nil
+	}
 }
 
-func OrQueryToDSL(q *lucene.OrQuery) (dsl.DSLNode, error) {
+func orQueryToDSLNode(q *lucene.OrQuery) (dsl.DSLNode, error) {
 	if q == nil {
 		return nil, EMPTY_OR_QUERY_ERR
 	}
-	return nil, nil
+	if node, err := andQueryToDSLNode(q.AndQuery); err != nil {
+		return nil, err
+	} else {
+		var nodes = []dsl.DSLNode{node}
+		for _, query := range q.AnSQuery {
+			if node, err := ansQueryToDSLNode(query); err != nil {
+				return nil, err
+			} else {
+				nodes = append(nodes, node)
+			}
+		}
+		return &dsl.AndDSLNode{MustNodes: nodes}, nil
+	}
 }
 
-func OsQueryToDSL(q *lucene.OSQuery) (dsl.DSLNode, error) {
+func osQueryToDSLNode(q *lucene.OSQuery) (dsl.DSLNode, error) {
 	if q == nil {
 		return nil, EMPTY_OR_QUERY_ERR
 	}
-	return nil, nil
+	return orQueryToDSLNode(q.OrQuery)
 }
 
-func AndQueryToDSL(q *lucene.AndQuery) (dsl.DSLNode, error) {
+func andQueryToDSLNode(q *lucene.AndQuery) (dsl.DSLNode, error) {
 	if q == nil {
 		return nil, EMPTY_AND_QUERY_ERR
 	}
-	return nil, nil
+	if q.FieldQuery != nil {
+		if node, err := fieldQueryToDSLNode(q.FieldQuery); err != nil {
+			return nil, err
+		} else if q.NotSymbol != nil {
+			return &dsl.NotDSLNode{Nodes: []dsl.DSLNode{node}}, nil
+		} else {
+			return node, nil
+		}
+	} else {
+		if node, err := parenQueryToDSLNode(q.ParenQuery); err != nil {
+			return nil, err
+		} else if q.NotSymbol != nil {
+			return &dsl.NotDSLNode{Nodes: []dsl.DSLNode{node}}, nil
+		} else {
+			return node, nil
+		}
+	}
 }
 
-func AnsQueryToDSL(q *lucene.AnSQuery) (dsl.DSLNode, error) {
+func ansQueryToDSLNode(q *lucene.AnSQuery) (dsl.DSLNode, error) {
 	if q == nil {
 		return nil, EMPTY_AND_QUERY_ERR
 	}
-	return nil, nil
+	return andQueryToDSLNode(q.AndQuery)
 }
 
-func ParenQueryToDSL(q *lucene.AnSQuery) (dsl.DSLNode, error) {
+func parenQueryToDSLNode(q *lucene.ParenQuery) (dsl.DSLNode, error) {
 	if q == nil {
 		return nil, EMPTY_PAREN_QUERY_ERR
 	}
-	return nil, nil
+	return luceneToDSLNode(q.SubQuery)
 }
 
-func FieldQueryToDSL(q *lucene.FieldQuery) (dsl.DSLNode, error) {
+// very import
+func fieldQueryToDSLNode(q *lucene.FieldQuery) (dsl.DSLNode, error) {
 	if q == nil {
 		return nil, EMPTY_FIELD_QUERY_ERR
 	}
