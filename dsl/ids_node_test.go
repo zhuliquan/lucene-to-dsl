8b1377528efc2378d658bdabfae06f3c8d32608a
zhuliquan <zlq164114@sina.com>
2023-02-06 21:52:59 +0800

    test: add id node test cases

--- dsl/ids_node_test.go
+++ dsl/ids_node_test.go
@@ -1,47 +1,63 @@
 package dsl
 
-// import (
-// 	"testing"
+import (
+	"testing"
 
-// 	"github.com/stretchr/testify/assert"
-// 	"github.com/zhuliquan/lucene-to-dsl/mapping"
-// )
+	"github.com/stretchr/testify/assert"
+	"github.com/zhuliquan/lucene-to-dsl/mapping"
+)
 
-// func TestIdsNode(t *testing.T) {
-// 	var node1 = &IdsNode{
-// 		ids: []string{"1", "2"},
-// 	}
-// 	var node2 = &IdsNode{
-// 		ids: []string{"2", "3"},
-// 	}
+func TestIdsNode(t *testing.T) {
+	var node1 = &IdsNode{
+		ids: []string{"1", "2"},
+	}
+	var node2 = &IdsNode{
+		ids: []string{"2", "3"},
+	}
 
-// 	assert.Equal(t, LEAF_NODE_TYPE, node1.AstType())
-// 	assert.Equal(t, IDS_DSL_TYPE, node1.DslType())
-// 	var node3, err = node1.InterSect(node2)
-// 	assert.Equal(t, &IdsNode{ids: []string{"2"}}, node3)
-// 	assert.Nil(t, err)
-// 	node3, err = node1.UnionJoin(node2)
-// 	assert.Equal(t, &IdsNode{ids: []string{"1", "2", "3"}}, node3)
-// 	assert.Nil(t, err)
+	assert.Equal(t, LEAF_NODE_TYPE, node1.AstType())
+	assert.Equal(t, IDS_DSL_TYPE, node1.DslType())
+	var node3, err = node1.InterSect(node2)
+	assert.Equal(t, &IdsNode{ids: []string{"2"}}, node3)
+	assert.Nil(t, err)
+	node3, err = node1.UnionJoin(node2)
+	assert.Equal(t, &IdsNode{ids: []string{"1", "2", "3"}}, node3)
+	assert.Nil(t, err)
 
-// 	var node4 = &MatchNode{
-// 		KvNode: KvNode{field: "foo", Value: Value{value: "bar", mType: mapping.TEXT_FIELD_TYPE}},
-// 	}
-// 	node3, err = node1.InterSect(node4)
-// 	assert.NotNil(t, err)
-// 	assert.Nil(t, node3)
+	var node4 = &MatchNode{
+		kvNode: kvNode{
+			fieldNode: fieldNode{
+				lfNode: lfNode{
+					filterCtxNode: filterCtxNode{
+						filterCtx: false,
+					},
+				},
+				field: "foo",
+			},
+			valueNode:valueNode{
+				valueType: valueType{
+					aType: false,
+					mType: mapping.TEXT_FIELD_TYPE,
+				},
+				value: "bar",
+			},
+		},
+	}
+	node3, err = node1.InterSect(node4)
+	assert.NotNil(t, err)
+	assert.Nil(t, node3)
 
-// 	node3, err = node1.UnionJoin(node4)
-// 	assert.NotNil(t, err)
-// 	assert.Nil(t, node3)
+	node3, err = node1.UnionJoin(node4)
+	assert.NotNil(t, err)
+	assert.Nil(t, node3)
 
-// 	node3, err = node1.Inverse()
-// 	assert.Nil(t, err)
-// 	assert.Equal(t, &NotNode{Nodes: map[string][]AstNode{
-// 		"LEAF:_id": {node1},
-// 	}}, node3)
+	node3, err = node1.Inverse()
+	assert.Nil(t, err)
+	assert.Equal(t, &NotNode{Nodes: map[string][]AstNode{
+		"_id": {node1},
+	}}, node3)
 
-// 	assert.Equal(t, "LEAF:_id", node1.NodeKey())
-// 	assert.Equal(t, DSL{"ids": DSL{"values": []string{"1", "2"}}}, node1.ToDSL())
+	assert.Equal(t, "_id", node1.NodeKey())
+	assert.Equal(t, DSL{"ids": DSL{"values": []string{"1", "2"}}}, node1.ToDSL())
 
-// }
+}
