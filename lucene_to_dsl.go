d5c64ca4db509f637c162f7a2dc7ec5b7389dd4d
zhuliquan <zlqlovecode@foxmail.com>
2026-06-13 23:06:39 +0800

    feat: set mapping and custom funcs with option

--- lucene_to_dsl.go
+++ lucene_to_dsl.go
@@ -2,41 +2,47 @@ package lucene_to_dsl
 
 import (
 	"fmt"
-	"sync"
 
+	mapping "github.com/zhuliquan/es-mapping"
 	"github.com/zhuliquan/lucene-to-dsl/convert"
 	"github.com/zhuliquan/lucene-to-dsl/dsl"
-	mapping "github.com/zhuliquan/es-mapping"
 	lucene "github.com/zhuliquan/lucene_parser"
 )
 
-var (
-	mappingPath string
+type Config struct {
+	mappingData []byte
 	customFuncs map[string]convert.ConvertFunc
-	onceInit    sync.Once
-	converter   convert.Converter
-)
-
-func LoadMappingPath(path string) {
-	mappingPath = path
 }
 
-func LoadCustomFuncs(funcs map[string]convert.ConvertFunc) {
-	customFuncs = funcs
+type Option func(*Config)
+
+func WithMappingData(data []byte) Option {
+	return func(o *Config) {
+		o.mappingData = data
+	}
 }
 
-func LuceneToDSL(luceneQuery string) (dsl.DSL, error) {
-	onceInit.Do(
-		func() {
-			if pm, err := mapping.LoadMappingFile(mappingPath); err != nil {
-				panic(err)
-			} else {
-				converter = convert.NewConverter(pm, customFuncs)
-			}
-		},
-	)
+func WithCustomConvertFunc(funcs map[string]convert.ConvertFunc) Option {
+	return func(o *Config) {
+		o.customFuncs = funcs
+	}
+}
 
-	var err error
+func LuceneToDSL(
+	query string,
+	opts ...Option,
+) (dsl.DSL, error) {
+	cfg := &Config{}
+	for _, opt := range opts {
+		opt(cfg)
+	}
+
+	pm, err := mapping.LoadMappingData(cfg.mappingData)
+	if err != nil {
+		return nil, fmt.Errorf("failed to load mapping data, err: %v", err)
+	}
+
+	var cvt = convert.NewConverter(pm, cfg.customFuncs)
 	var qry *lucene.Lucene
 	var nod dsl.AstNode
 	defer func() {
@@ -46,11 +52,11 @@ func LuceneToDSL(luceneQuery string) (dsl.DSL, error) {
 		}
 	}()
 
-	if qry, err = lucene.ParseLucene(luceneQuery); err != nil {
+	if qry, err = lucene.ParseLucene(query); err != nil {
 		return nil, err
 	}
 
-	if nod, err = converter.LuceneToAstNode(qry); err != nil {
+	if nod, err = cvt.LuceneToAstNode(qry); err != nil {
 		return nil, err
 	}
 
